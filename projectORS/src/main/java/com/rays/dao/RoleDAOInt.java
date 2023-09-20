73fb1f63b316b3a18d7d4bc489dfdf4d723df58d
kuldeepbadadwal <kuldeepkundal100@gmail.com>
2023-09-20 22:55:38 +0530

    success

---
+++ projectORS/src/main/java/com/rays/dao/RoleDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.RoleDTO;
+
+/**
+ * Role DAO interface.
+ * @author Kuldeep badadwal
+ */
+public interface RoleDAOInt extends BaseDAOInt<RoleDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Kuldeep badadwal
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
